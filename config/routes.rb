88f0f91c31aa8a37dcda5677cab4cef15a81e3f0
KevinAndrey96 <kaherreras@unal.edu.co>
2018-04-08 23:47:05 +0000

    Módulo de Anuncios

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :comments
   get 'search/index'
 
   get 'specific_report/index'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180408212807) do
+ActiveRecord::Schema.define(version: 20180408232945) do
 
   create_table "ar_internal_metadata", primary_key: "key", force: :cascade do |t|
     t.string   "value"
@@ -33,6 +33,12 @@ ActiveRecord::Schema.define(version: 20180408212807) do
     t.string   "name"
     t.string   "email"
     t.string   "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
