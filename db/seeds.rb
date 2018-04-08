4350f8d66d69869ea48d4e5f2dda4bf53a3742f1
KevinAndrey96 <kaherreras@unal.edu.co>
2018-04-08 23:57:31 +0000

    Estilo de anuncio

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Setting.create(exchange_rate: '0.09')
+Setting.create(text: 'Bienvenido a Colven Intercambios')
 User.create(email: 'admin@colvenintercambios.com', commission:0.05, zone:'Pruebas', role: 'Admin', name: 'Administrador', creator: 'admin@colvenintercambios.com', password: 'Pruebas2018@', password_confirmation: 'Pruebas2018@')
 User.create(email: 'wholesaler@colvenintercambios.com', commission:0.05, zone:'Pruebas', role: 'Wholesaler', name: 'Mayorista', creator: 'admin@colvenintercambios.com', password: 'Pruebas2018@', password_confirmation: 'Pruebas2018@')
 User.create(email: 'distributor@colvenintercambios.com', commission:0.05, zone:'Pruebas', role: 'Distributor', name: 'Distribuidor', creator: 'wholesaler@colvenintercambios.com', password: 'Pruebas2018@', password_confirmation: 'Pruebas2018@')
