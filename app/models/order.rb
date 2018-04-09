fa03db8bbc7d94c8e8375d808507908ec8e17eb8
KevinAndrey96 <kaherreras@unal.edu.co>
2018-04-09 02:20:36 +0000

    Validacion cuenta

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,5 @@ class Order < ActiveRecord::Base
   belongs_to :client
   mount_uploader :voucher, ImageUploader
   scope :desc, -> { order('id DESC') }
+  validates :account, length: { is: 20 }
 end
